ddfa99727fc83c3e8a83041be636daf99b1b3c57
Mitchel Carson <mitchel.carson@gmail.com>
2024-11-06 21:06:23 -0500

    Pattern printer that receives an integer as input and prints a pattern of integers and *'s.

---
+++ PatternPrinter.java
@@ -0,0 +1,35 @@
+package PatternPrinter;
+import java.util.Scanner;
+
+public class PatternPrinter {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter an interger: ");
+        int n = scan.nextInt();
+
+        // loop for the first half of the pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+                if (j != i) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+
+        // loop for the second half of the patter
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+                if (j != i) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}
